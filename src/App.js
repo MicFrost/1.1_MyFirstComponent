9f0030a209c8ece5b239078f9a3b8f51fb212b6e
MicFrost <116974794+MicFrost@users.noreply.github.com>
2023-01-05 21:57:20 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+function HipHip () {
+  return (
+   <h2>Hip Hip Hooray!</h2>
+    );
+}
+
+export default function Congratulations (){
+  return (
+    <div>
+    <h1>Good Job MicFrost!</h1>
+    <HipHip/>
+    <HipHip/>
+    <HipHip/>
+      </div>
+  );
+}
+  
